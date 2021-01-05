d67bb7884c2794f4d09b70e0f8249aa28a91d6eb
Kohei Yoshikawa <marimo3418@neko2.net>
2021-01-05 11:53:26 +0000

    define struct

--- func_h.h
+++ func_h.h
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stdio.h>
 
+#define NODES 10
 #define DEFAULT_COST 1
 
 void ba_init();
--- q1.cpp
+++ q1.cpp
@@ -1,6 +1,16 @@
 #include "func.cpp"
 using namespace std;
 
+typedef struct connect_t{
+    int next_node;
+    int cost;
+}_connect;
+
+typedef struct node_t{
+    int node_num;
+    _connect connect[NODES];
+}_node;
+
 void q1(){
     ba_init();
 }
