50d8a58c87d07a154028a1bae5e89fc6056b9678
Kohei Yoshikawa <marimo3418@neko2.net>
2021-01-09 08:32:23 +0000

    wrote FUNC define_connect_node

--- func_h.h
+++ func_h.h
@@ -8,4 +8,5 @@
 void ba_init();
 void node_put();
 void node_connection(int node_num, int node_connect);
+int define_connect_node();
 void print_list();
--- q1.cpp
+++ q1.cpp
@@ -13,12 +13,17 @@ typedef struct node_t{
 }_node;
 
 _node node[NODES]={};
-static int node_count=0;
+static int node_count=0, connect_node_total=0;
 
 void q1(){
     ba_init();
 
     for(int i=node_count;i<NODES;i++){
+        node_put();
+        node_count-=1;
+        define_connect_node();
+
+
 
         node_count+=1;
     }
@@ -33,27 +38,49 @@ void ba_init(){
     node_connection(0,1);
     node_connection(0,2);
     node_connection(1,2);
-}
+    connect_node_total+=6;
+}//Checked 2021.01.09 17.31
 
 void node_put(){
     node[node_count].node_num=node_count;
     node_count+=1;
-}
+}//Checked 2021.01.09 17.31
 
 void node_connection(int node_num, int node_connect){
     int top=node[node_num].connected_nodes;
     node[node_num].connect[top].next_node=node_connect;
     node[node_num].connect[top].cost=DEFAULT_COST;
     node[node_num].connected_nodes+=1;
+    connect_node_total+=1;
 
     top=node[node_connect].connected_nodes;
     node[node_connect].connect[top].next_node=node_num;
     node[node_connect].connect[top].cost=DEFAULT_COST;
     node[node_connect].connected_nodes+=1;
-}
+    connect_node_total+=1;
+}//Checked 2021.01.09 17.31
 
 int define_connect_node(){
-    
+    int node_sel=0;
+    long double rd_temp;
+    long double priority[node_count]={};
+
+    for(int i=0;i<node_count;i++){
+        priority[i]=(long double)node[i].connected_nodes/connect_node_total;
+    }
+
+    mt19937 rd{random_device{}()};
+    uniform_real_distribution<long double> dist(0, 1);
+    rd_temp=dist(rd);
+
+    do{
+        rd_temp-=priority[node_sel];
+        if(rd_temp<=0) break;
+        node_sel+=1;
+    }while(true);
+
+    cout << node_sel << endl;
+    return node_sel;
 }
 
 void print_list(){
@@ -67,4 +94,4 @@ void print_list(){
         cout << endl;
     }
     cout << endl;
-}
+}//Checked 2021.01.09 17.31
